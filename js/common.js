369d598594b588ec89620a7152821e17cf5e0c70
iwuooh <dbsal06@daum.net>
2020-11-18 16:35:09 +0900

    hash scroll animate update

--- js/common.js
+++ js/common.js
@@ -1,3 +1,12 @@
 $(function(){
   //script 입력영역
+  $("nav ul li a,.back_to_top a, .footer_top ul li a, a.button").click(function(e){
+    // console.log($(this.hash).offset().top)
+    var thisElem = $(this.hash);
+    $("html,body").stop();// 이전에 클릭한것을 멈춤
+    $("html,body").animate({scrollTop:thisElem.offset().top},1500);
+
+    // e.preventDefault(); //a tag return false해도 안될때
+    return false;
+  });
 });
